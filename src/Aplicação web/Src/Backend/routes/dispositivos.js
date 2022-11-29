ddd5bc90cc57855fbca1f45140aa61f4992f3a61
1zabella <99206817+1zabella@users.noreply.github.com>
2022-11-28 22:21:04 -0300

    estruturação do backend

--- src/Aplicação web/Src/Backend/routes/dispositivos.js
+++ src/Aplicação web/Src/Backend/routes/dispositivos.js
@@ -5,16 +5,15 @@ const express = require("express");
 const router = express.Router();
 // const {body, validationResult} = require("express-validator");
 
-
 const dispositivosController = require("../controllers/dispositivos");
 
 //ROTAS com seus respectivos controllers e middlewares
 
-router.post( "/create",  dispositivosController.novosDispositivos);
-// router.get( "/get", dispositivosController.createUser);
-// router.put( "/update",  dispositivosController.createUser);
-// router.delete( "/delete",  dispositivosController.createUser);
+router.post("/create", dispositivosController.novosDispositivos);
+router.get("/", dispositivosController.dispositivos);
+router.get("/unique", dispositivosController.dispositivoUnico);
+router.put( "/update",  dispositivosController.editarDispositivo);
+router.delete( "/delete",  dispositivosController.deletarDispositivo);
 
 // exportação das rotas
 module.exports = router;
-
